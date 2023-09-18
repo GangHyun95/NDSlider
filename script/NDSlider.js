674b4ff41b9abfb7e3a9818920ac0c8912499b4f
hyun <hgh6128@gmail.com>
2023-09-18 17:30:02 +0900

    연속해서 슬라이드를 넘길 시 currentIndex ++ , -- 로직 다르게 처리 new Date().getTime() 으로 startTime,endTime 구하여 시간 차를 이용함

--- script/NDSlider.js
+++ script/NDSlider.js
@@ -5,6 +5,7 @@ export default class NDSlider {
     #btnPrev;
     #btnNext;
     #currentIndex;
+
     constructor(selector, option = {}) {
         this.#slider = document.querySelector(selector);
         this.#wrapper = this.#slider.querySelector(".ndslider-wrapper");
@@ -16,18 +17,27 @@ export default class NDSlider {
         const { dragStart, dragging, dragEnd } = this.#dragHandlers();
         this.#wrapper.addEventListener("pointerdown", dragStart);
         document.addEventListener("pointermove", dragging);
-        document.addEventListener("pointerup", dragEnd.bind(this));
+        document.addEventListener("pointerup", dragEnd);
     }
 
     #dragHandlers() {
+        const parent = this;
         let isDragging = false,
             dragStartX = 0,
             currentTranslateX = 0,
+            recentlySwiped = false, // 최근에 슬라이드를 넘겼는지 체크
+            lastDragEndTime = 0,
+            startTime = 0,
             target;
+
         function dragStart(e) {
             isDragging = true;
             dragStartX = e.pageX;
+            startTime = new Date().getTime(); // 시작 시간 저장
             target = e.currentTarget;
+            
+            const deltaTime = new Date().getTime() - lastDragEndTime;
+            deltaTime < 300 ? recentlySwiped = true : recentlySwiped = false;
         }
 
         function dragging(e) {
@@ -40,21 +50,36 @@ export default class NDSlider {
 
         function dragEnd(e) {
             if (!isDragging) return;
-            currentTranslateX += e.pageX - dragStartX; // 드래그가 끝난 후에 최종 위치 저장
-            if (currentTranslateX) dragStartX = 0;
-            isDragging = false;
+            lastDragEndTime = new Date().getTime();
 
-            this.#currentIndex = Math.round(
-                -currentTranslateX / target.clientWidth
-            );
+            currentTranslateX += e.pageX - dragStartX;
+
+            if (recentlySwiped && Math.abs(e.pageX - dragStartX) > target.clientWidth * 0.05) {
+                if (e.pageX - dragStartX < 0) {
+                    // 오른쪽으로 드래그
+                    parent.#currentIndex++;
+                } else {
+                    // 왼쪽으로 드래그
+                    parent.#currentIndex--;
+                }
+            } else {
+                parent.#currentIndex = Math.round(
+                    -currentTranslateX / target.clientWidth
+                );
+            }
+
+            // 최소 및 최대 인덱스 제한
+            parent.#currentIndex = Math.min(Math.max(parent.#currentIndex, 0) , parent.#slides.length - 1);
 
             // 슬라이드 인덱스에 따라 위치 조정
-            currentTranslateX = -(this.#currentIndex * target.clientWidth);
+            currentTranslateX = -(parent.#currentIndex * target.clientWidth);
             target.style.transitionDuration = "0.3s";
             target.style.transform = `translate3d(${currentTranslateX}px, 0, 0)`;
             setTimeout(() => {
                 target.style.transitionDuration = "0s";
             }, 300);
+
+            isDragging = false;
         }
 
         return { dragStart, dragging, dragEnd };
